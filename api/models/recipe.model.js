cc52a58e162224ec40ae8c7773b8b6a36d9df4c7
Nan Yang <nanyang.sde@gmail.com>
2024-03-17 19:41:01 -0400

    added create recipe route -- not tested

--- api/models/recipe.model.js
+++ api/models/recipe.model.js
@@ -9,7 +9,7 @@ const recipeSchema = new mongoose.Schema({
     image: {
         type: String,
     },
-    tags: {
+    highlights: {
         type: [String], 
         required: true
         
--- api/routes/recipe.route.js
+++ api/routes/recipe.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { createRecipe } from "../controllers/recipe.controller";
 import e from "express";
-// import { verifyToken } from "../utils/verifyUser.js"
+import { verifyToken } from "../utils/verifyUser.js"
 
 const router = express.Router();
 
-router.post('/create-recipe',  createRecipe)
+router.post('/create-recipe', verifyToken, createRecipe)
 
 export default router;
