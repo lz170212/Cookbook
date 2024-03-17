39eac985d072482b9a67683e5ea71caa08b881fd
Nan Yang <nanyang.sde@gmail.com>
2024-03-17 19:57:40 -0400

    fixed typo

--- api/routes/recipe.route.js
+++ api/routes/recipe.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { createRecipe } from "../controllers/recipe.controller";
-import e from "express";
 import { verifyToken } from "../utils/verifyUser.js"
 
 const router = express.Router();
