a43b5cd2e765a16a8afb5ec02eb87a881b39c49e
amypxr <amy123pjw@gmail.com>
2024-03-19 17:50:03 -0400

    complete user profile update functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,47 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+
 export const test = (req,res)=>{
     res.json({
         message:"Api route is working",
     });
 };
+export const updateUser= async (req,res,next) =>{
+    //req.params.id => /api/user/update/:id
+    //req.user.id  in verifyUser.js after jwt verified set req.user=user of current session
+    //check can update or not
+    if(req.user.id != req.params.id){
+        return next(errorHandler(401, 'You can only update your own account!'));
+    }
+    //update
+    //crypt password
+    //connect to mongoDB find the data and update
+    try {
+        if (req.body.password) {
+          req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+    
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: {
+              username: req.body.username,
+              email: req.body.email,
+              password: req.body.password,
+              avatar: req.body.avatar,
+            },
+          },
+          { new: true } // return new version of doc
+        );
+        //response everything except password
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+
+      } catch (error) {
+        next(error);
+      }
+    
+
+
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,13 +9,22 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import {app} from '../firebase';
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser,error, loading } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] =useState(false);
+  const dispatch = useDispatch();
   //console.log(file);
   //console.log(formData);
 
@@ -48,10 +57,39 @@ export default function Profile() {
     );
   };
 
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+    });
+  };
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    try{
+    dispatch(updateUserStart());
+    const res = await fetch(`/api/user/update/${currentUser._id}`,{
+      method:"POST",
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      dispatch(updateUserFailure(data.message));
+      return;
+    }
+    dispatch(updateUserSuccess(data));
+    setUpdateSuccess(true);
+
+  }catch(error){
+    dispatch(updateUserFailure(error.message));
+  }
+  }
   return (
     <div className='p-3 mx-auto w-72'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <input  onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
         <img src={formData.avatar ||currentUser.avatar} onClick={()=>fileRef.current.click()} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
         <p className='text-sm self-center'>
@@ -67,11 +105,17 @@ export default function Profile() {
             ''
           )}
         </p>
-        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg'  />
-        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg'  />
-        <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg'  />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' defaultValue={currentUser.username} onChange={handleChange} />
+        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' defaultValue={currentUser.email} onChange={handleChange}/>
+        <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg'  onChange={handleChange}/>
 
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80' disabled={loading}>
+          { loading? 'Loading... ': 'Update'}
+          </button>
+        <p>
+          <span className='text-red-500'>{error? error : ''}</span>
+          <span className='text-green-500'>{updateSuccess? 'Updated Successfully':''}</span>
+        </p>
       </form>
       <div className="flex justify-between mt-5">
         <span className='text-red-700 cursor-pointer'>Delete account</span>
