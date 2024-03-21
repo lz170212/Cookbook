bd7fbbbdae6d63988d27016a41d501bc01e378d7
Nan Yang <nanyang.sde@gmail.com>
2024-03-21 15:27:16 -0400

    able to update database

--- api/controllers/recipe.controller.js
+++ api/controllers/recipe.controller.js
@@ -1,4 +1,5 @@
 import Recipe from '../models/recipe.model.js';
+import User from '../models/user.model.js'
 
 export const createRecipe = async (req, res, next) => {
 
@@ -16,7 +17,7 @@ export const getAllRecipes = async (req, res, next) => {
     let maxLimit = 6;
     try{
         let data = await Recipe.find()
-            .populate('author', "username -_id")
+            .populate('author', "username")
             .limit(maxLimit)
         res.status(200).json({data})
     } catch(err){
@@ -27,10 +28,23 @@ export const getAllRecipes = async (req, res, next) => {
 export const getRecipe = async (req, res, next) => {
     try{
         let data = await Recipe.findById(req.params.id )
-            .populate('author', "username -_id")
+            .populate('author', "username")
         res.status(200).json({data})
     } catch(err){
         next(err)
     }
 }
 
+export const saveRecipe = async (req, res, next) => {
+    try{
+        let { userId, recipeId } = req.body;
+        await User.findOneAndUpdate({_id: userId}, { $push: {"saved_recipes": recipeId}})
+       
+        await Recipe.findOneAndUpdate({ _id: recipeId}, { $inc: {"total_collect": 1}})
+
+        res.status(200).json({ result: "success" })
+
+    } catch(err){
+        next(err)
+    }
+}
--- api/routes/recipe.route.js
+++ api/routes/recipe.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createRecipe, getAllRecipes, getRecipe  } from '../controllers/recipe.controller.js'
+import { createRecipe, getAllRecipes, getRecipe, saveRecipe } from '../controllers/recipe.controller.js'
 
 const router = express.Router();
 
 router.post('/create-recipe', createRecipe)
 router.get('/get', getAllRecipes)
 router.get('/get/:id', getRecipe)
+router.post('/save-recipe', saveRecipe)
 
 
 
