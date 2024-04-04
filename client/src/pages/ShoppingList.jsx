8419fe62ebf5ccf2c6756e2382b5a565df8fbbd7
Nan Yang <nanyang.sde@gmail.com>
2024-04-04 17:07:02 -0400

    basic layout for shopping list

--- client/src/pages/ShoppingList.jsx
+++ client/src/pages/ShoppingList.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState, createContext } from 'react';
+import IngredientItem from '../components/IngredientItem';
+
+// export const ShoppingListContext = createContext({})
 
 export default function ShoppingList() {
+
+  const [ ingredients, setIngredients ] = useState(null)
+
+  const extractIngredients = ( weekly_menu ) => {
+    // { name: { quantity: xxx, recipes: {} } }
+    
+    let result = { } 
+    for (let i = 0; i < weekly_menu.length; i++){
+      let { name: recipe, ingredients } = weekly_menu[i].menu
+      for (let j = 0 ; j < ingredients.length; j ++){
+        let array = ingredients[j].split(' ')
+        let ingredient = array[array.length - 1]
+        let quantity = isNaN(Number(ingredients[j].split(' ')[0])) ? 0.1 : Number(ingredients[j].split(' ')[0])
+
+        if (ingredient in result){
+          
+          result[ingredient].quantity += quantity
+          if (!recipe in result[ingredient].recipes){
+            result[ingredient].recipes[recipe] = 1
+          } else {
+            result[ingredient].recipes[recipe] += 1
+          }
+          
+        } else {
+          result[ingredient] = { quantity, recipes: {[recipe]: 1} }
+        }
+      }
+      
+      setIngredients(result)
+    }
+
+
+
+  }
+
+  const fetchWeeklyMenu = async () => {
+    try{
+      const res = await fetch('/api/recipe/weekly-menu')
+      const { weekly_menu } = await res.json()
+      extractIngredients(weekly_menu)
+
+    } catch(err){
+      console.log(err.message)
+    }
+ 
+  }
+
+  useEffect(() => {
+    fetchWeeklyMenu()
+  }, [])
+
   return (
-    <div>ShoppingList</div>
+    // <ShoppingListContext.Provider value={{ ingredients, setIngredients }}>
+    <div className='w-full font-montserrat mx-10'>
+      
+      <h1 className='font-bold text-blue-500 text-2xl mx-auto my-6'>Groceries for the Week</h1>
+      
+      <div className=''>
+        {
+          ingredients ? 
+          Object.keys(ingredients).map((key, i) => {
+            return <IngredientItem key={i} item={key} detail={ingredients[key]} index={i} />
+          })
+          : ""
+        }
+
+      </div>
+    </div>
+    // </ShoppingListContext.Provider>
   )
 }
